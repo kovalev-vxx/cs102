85666065119c8c117145170e0824f42fc9b79799
kovalev-vxx <>
2020-10-10 12:18:50 +0300

    GET_KEY is Ready

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -10,7 +10,28 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+
+    s = list(keyword)  # create list keyword
+    key = ''
+    i = 0
+    # -----------GET KEY------------#
+    if len(plaintext) > len(keyword):
+        div = len(plaintext) // len(keyword)
+        time_word = keyword * div
+        dif = len(plaintext) - len(time_word)  # difference between plainttext and time_word
+        a = list(time_word)  # create list time_word
+        while i < dif:
+            a.append(a[i])
+            i += 1
+        key = ''.join(a)
+    elif len(plaintext) < len(keyword):
+        len_key = len(plaintext)
+        for i in range(0, len_key):
+            key += s[i]
+    else:
+        key = keyword
+    # -----------------------------#
+
     return ciphertext
 
 
