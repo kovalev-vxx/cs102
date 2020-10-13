8b405818e5217276f1265ee4560d553df6f61459
kovalev-vxx <>
2020-10-14 01:02:38 +0300

    RSA IS READY

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -5,7 +5,6 @@ import typing as tp
 def is_prime(n: int) -> bool:
     """
     Tests to see if a number is prime.
-
     >>> is_prime(2)
     True
     >>> is_prime(11)
@@ -13,17 +12,16 @@ def is_prime(n: int) -> bool:
     >>> is_prime(8)
     False
     """
-
     i = 1
     m = 0
 
     if n > 1:
         while i <= n:
-            if n/i == n//i:
-                i+=1
-                m+=1
+            if n / i == n // i:
+                i += 1
+                m += 1
             else:
-                i+=1
+                i += 1
     else:
         return False
 
@@ -36,26 +34,35 @@ def is_prime(n: int) -> bool:
 def gcd(a: int, b: int) -> int:
     """
     Euclid's algorithm for determining the greatest common divisor.
-
     >>> gcd(12, 15)
     3
     >>> gcd(3, 7)
     1
     """
-    # PUT YOUR CODE HERE
-    pass
+    while a != 0 and b != 0:
+        if a > b:
+            a = a % b
+        else:
+            b = b % a
+    return a + b
 
 
 def multiplicative_inverse(e: int, phi: int) -> int:
     """
     Euclid's extended algorithm for finding the multiplicative
     inverse of two numbers.
-
     >>> multiplicative_inverse(7, 40)
     23
     """
-    # PUT YOUR CODE HERE
-    pass
+    N = phi
+    x, xx, y, yy = 1, 0, 0, 1
+    while e:
+        q = phi // e
+        phi, e = e, phi % e
+        x, xx = xx, x - xx * q
+        y, yy = yy, y - yy * q
+    d = y % N
+    return d
 
 
 def generate_keypair(p: int, q: int) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[int, int]]:
@@ -65,10 +72,10 @@ def generate_keypair(p: int, q: int) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[in
         raise ValueError("p and q cannot be equal")
 
     # n = pq
-    # PUT YOUR CODE HERE
+    n = p * q
 
     # phi = (p-1)(q-1)
-    # PUT YOUR CODE HERE
+    phi = (p - 1) * (q - 1)
 
     # Choose an integer e such that e and phi(n) are coprime
     e = random.randrange(1, phi)
