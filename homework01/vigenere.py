c646b4d6e53b4be1a1d80ef891ae7c42d7a3ddac
kovalev-vxx <>
2020-10-13 14:31:21 +0300

    def decrypt_vigenere() is Ready

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -87,5 +87,67 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     'ATTACKATDAWN'
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+
+    s = list(keyword)  # create list keyword
+    key = ""
+    i = 0
+    # -----------GET KEY------------#
+    if len(ciphertext) > len(keyword):
+        div = len(ciphertext) // len(keyword)
+        time_word = keyword * div
+        dif = len(ciphertext) - len(time_word)  # difference between plainttext and time_word
+        a = list(time_word)  # create list time_word
+        while i < dif:
+            a.append(a[i])
+            i += 1
+        key = "".join(a)
+    elif len(ciphertext) < len(keyword):
+        len_key = len(ciphertext)
+        for i in range(0, len_key):
+            key += s[i]
+    else:
+        key = keyword
+    # -----------------------------#
+
+
+    key = key.lower()  # transform key to lower register
+
+    id_symb = list()  # create list with symbols ID
+    shifts = list()  # create list with shifts
+
+    for m in key:
+        shifts.append(ord(m) - 97)  # fill shifts list
+
+    for m in ciphertext:  # fill symbols ID list
+        id_symb.append(ord(m))
+
+    # ---------------GET ENCRYPT WORD-------------- #
+
+    ciph = list()
+
+    for x, y in zip(id_symb, shifts):  # 65 - 90(A-Z), 97 - 122 (a-z)
+        if (65 + y) <= x <= 90:
+            w = x - y
+            ciph.append(w)
+        elif (97 + y) <= x <= 122:
+            w = x - y
+            ciph.append(w)
+        elif 65 <= x <= (65 + y):
+            w = abs(x-y-65)
+            ciph.append(91 - w)
+        elif 97 <= x <= (97+y):
+            w = abs(x-y-97)
+            ciph.append(123-w)
+        else:
+            ciph.append(x)
+
+    decrypt_word = list()
+
+    for m in ciph:
+        decrypt_word.append(chr(m))
+
+    plaintext = "".join(decrypt_word)
+
+    # ----------------------------- #
+
     return plaintext
