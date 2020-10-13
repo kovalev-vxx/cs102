e7e5cef9f196901166913d35335d5195881d339a
kovalev-vxx <>
2020-10-13 15:10:02 +0300

    CAESAR 2.0 IS READY

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -15,21 +15,35 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     ''
     """
     ciphertext = ""
+    id_symb = list()  # create list with symbols ID
+    encrypt_ID = list()  # create list with encrypt symbols ID
+
     for i in plaintext:
-        if i.isupper() and i.isalpha() and ord(i) > 90 - shift:  # 65 - 90(A-Z), 97 - 122 (a-z)
-            a = 90 - ord(i)
-            ciphertext += chr(a + 65)
-        elif i.isupper() and i.isalpha():
-            a = ord(i)
-            ciphertext += chr(a + shift)
-        elif i.lower() and ord(i) > 122 - shift and i.isalpha():
-            a = 122 - ord(i)
-            ciphertext += chr(a + 97)
-        elif i.lower() and i.isalpha():
-            a = ord(i)
-            ciphertext += chr(a + shift)
+        id_symb.append(ord(i))
+
+    for i in id_symb:  # 65 - 90(A-Z), 97 - 122 (a-z)
+        if 65 <= i <= (90 - shift):
+            w = i + shift
+            encrypt_ID.append(w)
+        elif 97 <= i <= (122 - shift):
+            w = i + shift
+            encrypt_ID.append(w)
+        elif (90 - shift) <= i <= 97:
+            w = 90 - i
+            encrypt_ID.append(64 + shift - w)
+        elif (97 - shift) <= i <= 122:
+            w = 122 - i
+            encrypt_ID.append(96 + shift - w)
         else:
-            ciphertext += i
+            encrypt_ID.append(i)
+
+        encrypt_word = list()
+
+        for m in encrypt_ID:  # transform ID to symbols
+            encrypt_word.append(chr(m))
+
+        ciphertext = "".join(encrypt_word)
+
     return ciphertext
 
 
@@ -47,21 +61,34 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     ''
     """
     plaintext = ""
-    for i in ciphertext:
-        if i.isupper() and i.isalpha() and ord(i) - shift >= 65:  # 65 - 90(A-Z), 97 - 122 (a-z)
-            a = ord(i) - shift
-            plaintext += chr(a)
-        elif i.isupper() and i.isalpha():
-            a = 90 - ord(i)
-            plaintext += chr(a + 65)
-        elif i.islower() and i.isalpha() and ord(i) - shift >= 97:
-            a = ord(i) - shift
-            plaintext += chr(a)
-        elif i.islower() and i.isalpha():
-            a = 122 - ord(i)
-            plaintext += chr(a + 97)
+    id_symb = list()  # create list with symbols ID
+    decrypt_ID = list()  # create list with decrypt symbols ID
+
+    for m in ciphertext:
+        id_symb.append(ord(m))
+
+    for i in id_symb:  # 65 - 90(A-Z), 97 - 122 (a-z)
+        if (65 + shift) <= i <= 90:
+            w = i - shift
+            decrypt_ID.append(w)
+        elif (97 + shift) <= i <= 122:
+            w = i - shift
+            decrypt_ID.append(w)
+        elif 65 <= i <= (65 + shift):
+            w = abs(i - shift - 65)
+            decrypt_ID.append(91 - w)
+        elif 97 <= i <= (97 + shift):
+            w = abs(i - shift - 97)
+            decrypt_ID.append(123 - w)
         else:
-            plaintext += i
+            decrypt_ID.append(i)
+
+    decrypt_word = list()
+
+    for i in decrypt_ID:  # transform ID to symbols
+        decrypt_word.append(chr(i))
+
+    plaintext = "".join(decrypt_word)
 
     return plaintext
 
@@ -71,5 +98,5 @@ def caesar_breaker_brute_force(ciphertext: str, dictionary: tp.Set[str]) -> int:
     Brute force breaking a Caesar cipher.
     """
     best_shift = 0
-    # PUT YOUR CODE HERE
+
     return best_shift
