c9240ee8bf11901b66fb27dff9cd1433d5654371
kovalev-vxx <>
2020-11-03 21:57:36 +0300

    mypy

--- homework02/sudoku.py
+++ homework02/sudoku.py
@@ -67,11 +67,12 @@ def get_col(grid: List[List[str]], pos: Tuple[int, int]) -> List[str]:
     >>> get_col([['1', '2', '3'], ['4', '5', '6'], ['.', '8', '9']], (0, 2))
     ['3', '6', '9']
     """
-    actual_col: List[str] = []
+    actual_col = []
     row, col = pos
-    for row in grid:
-        for i in row[col]:
-            actual_col.append(str(i))
+    get_grid = grid
+    for m in get_grid:
+        for i in m[col]:
+            actual_col.append(i)
 
     return actual_col
 
@@ -102,8 +103,8 @@ def get_block(grid: List[List[str]], pos: Tuple[int, int]) -> List[str]:
     while col % 3 != 0:
         col += 1
 
-    for i in grid[row - 3 : row]:
-        for m in i[col - 3 : col]:
+    for i in grid[row - 3: row]:
+        for m in i[col - 3: col]:
             expected_block.append(m)
 
     return expected_block
@@ -119,7 +120,6 @@ def find_empty_positions(grid: List[List[str]]) -> Optional[Tuple[int, int]]:
     >>> find_empty_positions([['1', '2', '3'], ['4', '5', '6'], ['.', '8', '9']])
     (2, 0)
     """
-
     for i in grid:
         for m in i:
             if m == ".":
@@ -127,6 +127,7 @@ def find_empty_positions(grid: List[List[str]]) -> Optional[Tuple[int, int]]:
                 col = grid.index(i)
                 pos = col, row
                 return pos
+    return None
 
 
 def find_possible_values(grid: List[List[str]], pos: Tuple[int, int]) -> Set[str]:
@@ -140,13 +141,11 @@ def find_possible_values(grid: List[List[str]], pos: Tuple[int, int]) -> Set[str
     >>> values == {'2', '5', '9'}
     True
     """
-    row, col = pos
-    values: Set[str] = {"1", "2", "3", "4", "5", "6", "7", "8", "9"}
-    for i in grid[row]:
+    values = {"1", "2", "3", "4", "5", "6", "7", "8", "9"}
+    for i in get_row(grid, pos):
         values.discard(i)
-    for i in grid:
-        for m in i[col]:
-            values.discard(m)
+    for i in get_col(grid, pos):
+        values.discard(i)
     for i in get_block(grid, pos):
         values.discard(i)
 
@@ -167,18 +166,21 @@ def solve(grid: List[List[str]]) -> Optional[List[List[str]]]:
     [['5', '3', '4', '6', '7', '8', '9', '1', '2'], ['6', '7', '2', '1', '9', '5', '3', '4', '8'], ['1', '9', '8', '3', '4', '2', '5', '6', '7'], ['8', '5', '9', '7', '6', '1', '4', '2', '3'], ['4', '2', '6', '8', '5', '3', '7', '9', '1'], ['7', '1', '3', '9', '2', '4', '8', '5', '6'], ['9', '6', '1', '5', '3', '7', '2', '8', '4'], ['2', '8', '7', '4', '1', '9', '6', '3', '5'], ['3', '4', '5', '2', '8', '6', '1', '7', '9']]
     """
 
-    for row in range(9):
-        for col in range(9):
-            if grid[row][col] == ".":
-                for x in list(find_possible_values(grid, (row, col))):
-                    grid[row][col] = x
-                    solution = solve(grid)
-                    if solution:
-                        return solution
-                    else:
-                        grid[row][col] = "."
-                return
-    return grid
+    empty_position = find_empty_positions(grid)
+
+    if empty_position is None:
+        return grid
+    else:
+        row, col = empty_position
+
+    for x in list(find_possible_values(grid, (row, col))):
+        grid[row][col] = x
+        solution = solve(grid)
+        if solution:
+            return solution
+        else:
+            grid[row][col] = "."
+    return None
 
 
 def check_solution(solution: List[List[str]]) -> bool:
@@ -189,23 +191,13 @@ def check_solution(solution: List[List[str]]) -> bool:
     for row in range(9):
         for col in range(9):
             pos = row, col
-            block = set()
-            row_solution = set()
-            col_solution = set()
             if not solution[row][col] in values:
                 return False
-            for i in get_block(solution, pos):
-                block.add(i)
-            if block != values:
+            if set(get_block(solution, pos)) != values:
                 return False
-            for i in solution[row]:
-                row_solution.add(i)
-            if row_solution != values:
+            if set(get_row(solution, pos)) != values:
                 return False
-            for i in solution:
-                for m in i[col]:
-                    col_solution.add(m)
-            if col_solution != values:
+            if set(get_col(solution, pos)) != values:
                 return False
     return True
 
@@ -242,20 +234,20 @@ def generate_sudoku(N: int) -> List[List[str]]:
         for col in range(9):
             empty_grid.append(".")
     random_grid = group(empty_grid, 9)
+
     solution = solve(random_grid)
 
-    for i in solution:
-        for m in i:
-            new_grid.append(m)
+    if solution is not None:
+        for i in solution:
+            for m in i:
+                new_grid.append(m)
 
     while z != max_sudoku_numbers - N:
         a = random.choice(new_grid)
         if a != ".":
             new_grid[new_grid.index((a))] = "."
             z += 1
-
-    if group(new_grid, 9) is not None:
-        return group(new_grid, 9)
+    return group(new_grid, 9)
 
 
 if __name__ == "__main__":
