18f40d7583e57ef8a34cc0dc87287c4050aeed8a
Valery Kovalev <kovalev@MacBook-Pro-Valery.local>
2021-01-22 01:15:49 +1000

    some changes

--- homework05/vkapi/config.py
+++
@@ -1,7 +0,0 @@
-# https://hackernoon.com/4-ways-to-manage-the-configuration-in-python-4623049e841b
-
-VK_CONFIG = {
-    "domain": "https://api.vk.com/method",
-    "access_token": "ce9788cc124808b106457c4c5149420e6c7178f4b741755fb701c7606fc13428f03cb87efa63b7f6139c9",
-    "version": "5.126",
-}
