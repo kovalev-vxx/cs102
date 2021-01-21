0da69f05dce16c10933f35ec5746cb3b2792e878
Valery Kovalev <kovalev@MacBook-Pro-Valery.local>
2021-01-21 23:52:38 +1000

    add config.py

--- homework05/vkapi/config.py
+++ homework05/vkapi/config.py
@@ -2,6 +2,6 @@
 
 VK_CONFIG = {
     "domain": "https://api.vk.com/method",
-    "access_token": "",
+    "access_token": "47cf7770bd93280c70937ee8bf81538c6755528e14d13740f9af99926cb245a84d195407256b3a174aaf8",
     "version": "5.126",
 }
