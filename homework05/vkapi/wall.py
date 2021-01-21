b39171a87a6430062ce51785d8b636582d92a9f2
Valery Kovalev <kovalev@MacBook-Pro-Valery.local>
2021-01-21 23:14:34 +1000

    wall.py is Ready

--- homework05/vkapi/wall.py
+++ homework05/vkapi/wall.py
@@ -1,13 +1,15 @@
 import textwrap
 import time
 import typing as tp
+from itertools import chain
 from string import Template
 
-import pandas as pd
-from pandas import json_normalize
+import pandas as pd  # type: ignore
+from pandas import json_normalize  # type: ignore
+from tqdm import tqdm  # type: ignore
 
-from vkapi import config, session
-from vkapi.exceptions import APIError
+from vkapi import config, session  # type: ignore
+from vkapi.exceptions import APIError  # type: ignore
 
 
 def get_posts_2500(
@@ -20,7 +22,35 @@ def get_posts_2500(
     extended: int = 0,
     fields: tp.Optional[tp.List[str]] = None,
 ) -> tp.Dict[str, tp.Any]:
-    pass
+
+    if count > max_count:
+        count = max_count
+
+    count_of_iter = (count + 99) // 100
+    code = (
+        f"{{"
+        f"var doc = [];"
+        f"var request_count = {count};"
+        f"var i=0;while (i < {count_of_iter}){{"
+        f"  var offset = {offset} + i * 100;"
+        f"  var docs = API.wall.get({{'owner_id': '{owner_id}', 'extended': '{extended}', 'domain': '{domain}','count': request_count,'offset': offset, 'filter': 'owner','v': '{config.VK_CONFIG['version']}'}});"
+        f"  if (request_count > 100) {{request_count = request_count - 100;}}"
+        f"  doc.push(docs.items);"
+        f"  i = i+1;}}"
+        f"return doc;}}"
+    )
+
+    response = session.post(
+        url="execute",
+        data={"code": code, "access_token": config.VK_CONFIG["access_token"], "v": "5.126"},
+        timeout=20.0,
+    ).json()
+
+    if "error" in response:
+        raise APIError(response["error"]["error_msg"])
+
+    response = list(chain.from_iterable(response["response"]))
+    return response
 
 
 def get_wall_execute(
@@ -49,4 +79,58 @@ def get_wall_execute(
     :param fields: Список дополнительных полей для профилей и сообществ, которые необходимо вернуть.
     :param progress: Callback для отображения прогресса.
     """
-    pass
+
+    def get_wall_info(code: str):
+        response = session.post(
+            url="execute",
+            data={
+                "code": code,
+                "access_token": config.VK_CONFIG["access_token"],
+                "v": config.VK_CONFIG["version"],
+            },
+            timeout=20.0,
+        ).json()
+        if "error" in response:
+            raise APIError(response["error"]["error_msg"])
+        else:
+            return response["response"]
+
+    total_count = get_wall_info(
+        f"""var docs=API.wall.get({{"domain":"{domain}","count":"{count}"}});return docs.count;"""
+    )
+    time.sleep(1)
+    if count == 0:
+        count = total_count
+    elif count <= 100:
+        code = f"""var docs=API.wall.get({{"owner_id":"{owner_id}","extended":"{extended}","domain":"{domain}","count":"{count}","offset":"{offset}","filter":"{filter}"}});return docs;"""
+        return json_normalize(get_wall_info(code)["items"])
+
+    if count % max_count == 0:
+        count_of_iter = count // max_count
+    else:
+        count_of_iter = count // max_count + 1
+
+    posts = []
+    with tqdm(total=count, disable=False if progress else True) as pbar:
+        for i in range(0, count_of_iter):
+            offset = offset + max_count * i
+            response = get_posts_2500(
+                count=count,
+                owner_id=owner_id,
+                domain=domain,
+                offset=offset,
+                max_count=max_count,
+                filter=filter,
+                extended=extended,
+            )
+            count = count - max_count
+
+            for j in response:
+                posts.append(j)
+                pbar.update(1)
+
+            if count_of_iter > 1:
+                time.sleep(1 / 3)
+
+    pbar.close()
+    return json_normalize(posts)
