72b34ac35ccb6da5053dd0cdacf789228d6cc4fb
Valery Kovalev <kovalev@MacBook-Pro-Valery.local>
2021-01-22 11:35:39 +1000

    config.py fixed

--- homework05/vkapi/config.py
+++ homework05/vkapi/config.py
@@ -2,6 +2,6 @@
 
 VK_CONFIG = {
     "domain": "https://api.vk.com/method",
-    "access_token": "ce9788cc124808b106457c4c5149420e6c7178f4b741755fb701c7606fc13428f03cb87efa63b7f6139c9",
+    "access_token": "",
     "version": "5.126",
 }
