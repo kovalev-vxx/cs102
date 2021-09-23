9a1effe3690a4a42cf0dbefbb4ecd39c5443ed93
kovalev-vxx <kovalev.vxx@gmail.com>
2021-09-23 12:28:10 +0300

    TCPServer(). Начало. Запускатеся

--- homework07-web/httpserver/httpserver/server.py
+++ homework07-web/httpserver/httpserver/server.py
@@ -2,7 +2,7 @@ import socket
 import threading
 import typing as tp
 
-from .handlers import BaseRequestHandler
+#from .handlers import BaseRequestHandler
 
 
 class TCPServer:
@@ -13,14 +13,14 @@ class TCPServer:
         backlog_size: int = 1,
         max_workers: int = 1,
         timeout: tp.Optional[float] = None,
-        request_handler_cls: tp.Type[BaseRequestHandler] = BaseRequestHandler,
+        #request_handler_cls: tp.Type[BaseRequestHandler] = BaseRequestHandler,
     ) -> None:
         self.host = host
         self.port = port
         self.server_address = (host, port)
         # @see: https://stackoverflow.com/questions/36594400/what-is-backlog-in-tcp-connections
         self.backlog_size = backlog_size
-        self.request_handler_cls = request_handler_cls
+        #self.request_handler_cls = request_handler_cls
         self.max_workers = max_workers
         self.timeout = timeout
         self._threads: tp.List[threading.Thread] = []
@@ -29,7 +29,27 @@ class TCPServer:
         # @see: http://veithen.io/2014/01/01/how-tcp-backlog-works-in-linux.html
         # @see: https://en.wikipedia.org/wiki/Thundering_herd_problem
         # @see: https://stackoverflow.com/questions/17630416/calling-accept-from-multiple-threads
-        pass
+        address = (self.host, self.port)
+        serv_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        serv_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
+        serv_socket.bind(address)
+        serv_socket.listen(self.backlog_size)
+
+        print(f"Listening on http://{address[0]}:{address[1]}/")
+
+        try:
+            while True:
+                client_sock, client_addr = serv_socket.accept()
+                print(f"client: {client_sock}, addr: {client_addr}")
+
+                while True:
+                    data = client_sock.recv(1024)
+                    if not data:
+                        break
+                    client_sock.sendall(data)
+                client_sock.close()
+        except KeyboardInterrupt:
+            print("Good bye...")
 
     def handle_accept(self, server_socket: socket.socket) -> None:
         pass
@@ -37,3 +57,7 @@ class TCPServer:
 
 class HTTPServer(TCPServer):
     pass
+
+if __name__ == "__main__":
+    server = TCPServer(host="localhost", port=5050)
+    server.serve_forever()
